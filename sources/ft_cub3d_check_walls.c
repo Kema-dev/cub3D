9101383b94cac61c55775c9e73ae45490950225e
Kema-dev <jeremy.jdn@free.fr>
2021-02-05 15:59:39 +0100

    fix X circle

--- sources/ft_cub3d_check_walls.c
+++ sources/ft_cub3d_check_walls.c
@@ -6,7 +6,7 @@
 /*   By: jjourdan <jjourdan@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/05 15:26:51 by jjourdan          #+#    #+#             */
-/*   Updated: 2021/02/05 15:47:25 by jjourdan         ###   ########lyon.fr   */
+/*   Updated: 2021/02/05 15:58:58 by jjourdan         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,16 +30,24 @@ char						**ft_cub3d_get_test_map(char **map)
 	len = ft_strlen(map[0]);
 	if (!(test_map[0] = ft_calloc(len + 1, sizeof(char))))
 		return (NULL);
-	if (!(test_map[nb_rows] = ft_calloc(len + 1, sizeof(char))))
-		return (NULL);
 	ft_memset(test_map[0], 'X', len);
-	ft_memset(test_map[nb_rows], 'X', len);
-	while (--nb_rows > 0)
+	len = ft_strlen(map[nb_rows]);
+	if (!(test_map[nb_rows + 2] = ft_calloc(len + 1, sizeof(char))))
+		return (NULL);
+	ft_memset(test_map[nb_rows + 2], 'X', len);
+	printf("=%s=\n", test_map[nb_rows+2]);
+	while (nb_rows > 0)
 	{
 		if (!(test_map[nb_rows] = ft_calloc(len = ft_strlen(map[nb_rows - 1]) + 1, sizeof(char))))
 			return (NULL);
 		while (--len >= 0)
-			test_map[nb_rows][len] = map[nb_rows - 1][len];
+		{
+			if (map[nb_rows - 1][len] == ' ')
+				test_map[nb_rows][len] = 'X';
+			else
+				test_map[nb_rows][len] = map[nb_rows - 1][len];
+		}
+		nb_rows--;
 	}
 	len = -1;
 	while (test_map[++len])
@@ -56,4 +64,8 @@ int							ft_cub3d_check_walls(t_map_params *map_params, \
 
 	if (!(test_map = ft_cub3d_get_test_map(map_params->map)))
 		return (MALLOC_FAIL);
+	(void)empty;
+	(void)wall;
+	(void)sprite;
+	return (SUCCESS);
 }
